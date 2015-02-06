46f962bb408d420bfd91ebd9484183f7e2988df9
Team293 <293.First@gmail.com>
2015-02-06 18:13:42 -0500

    Revert ":sailboat: /play ohyeah"
    This reverts commit 0aa9f94d01ca61d5a1f74fca22fcc30dfdf70783.

--- src/subsystems/Elevator.java
+++ src/subsystems/Elevator.java
@@ -13,14 +13,13 @@ public class Elevator {
 	private static final Encoder encoder = new Encoder(Ports.elevatorEncoder1, Ports.elevatorEncoder2);
 	private static final DigitalInput topLimit = new DigitalInput(Ports.topLimit);
 	private static final DigitalInput bottomLimit = new DigitalInput(Ports.bottomLimit);
-//	private static final PIDController pid = new PIDController(0.1, 0.1, 0.1, encoder, elevator);
+	private static final PIDController pid = new PIDController(0.1, 0.1, 0.1, encoder, elevator);
 	private static boolean manualMode = true;
-	private static double position = 0;
-	static double[] positions = new double[] {234,2355,2533};
-	private static final double tolerance = 128;
-	private static double output = 0;
-	private static double error = 0;
-/*	
+	private static int position = 0;
+	static int[] positions = new int[] {234,2355,2533};
+	private static final int tolerance = 128;
+
+	
 	private class pidElevatorControl implements PIDOutput {
 		public void pidWrite(double output) {
 			if (topLimit.get() && output > 0) {
@@ -32,12 +31,9 @@ public class Elevator {
 			}
 		}
 	}
-*/	
+	
 	public static void move(double speed) {
 		elevator.set(speed);
-	}
-	public static double getSpeed(){
-		return output;
 	}
 
 	public static void setMode(boolean newMode) {
@@ -51,21 +47,9 @@ public class Elevator {
 	public static void presetPosition(int positionInput) {
 		position = positions[positionInput];
 	}
-	public static double getError(){
-		error= encoder.getDistance()-position;
-		return error;
-	}
-	public static double getSetSpeed(double position2){
-		output=1.0*error/Math.abs(error);
-		if (Math.abs(error)<tolerance){
-			output = 0;
-		}
-		return output;
-	}
+
 	public static void goToPosition() {
-		elevator.set(getSetSpeed(position));
-		if (topLimit.get() || bottomLimit.get()){
-			elevator.set(0);
-		}
+		pid.setAbsoluteTolerance(tolerance);
+		pid.setSetpoint(position);
 	}
 }
