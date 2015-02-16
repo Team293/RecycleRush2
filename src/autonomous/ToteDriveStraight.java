d0f83419350c227b8d34eca0e6f923c4ae128681
Team293 <293.First@gmail.com>
2015-02-16 12:12:24 -0500

    :lollipop: Chris
    added ToteDriveStraight autonomous

--- src/autonomous/ToteDriveStraight.java
+++ src/autonomous/ToteDriveStraight.java
@@ -1,5 +1,26 @@
 package autonomous;
 
-public class ToteDriveStraight {
+import subsystems.DriveTrain;
+import subsystems.Elevator;
 
-}
+public class ToteDriveStraight {
+	static double distance = 0;
+	static int state = 0;
+	
+	public static void init() {
+		DriveTrain.enable();
+	}
+	
+	public static void drive() {
+		distance = DriveTrain.getAvgDistance();
+		
+		if (state == 0)  {
+			DriveTrain.tankDrive(.5, .5);
+			state = 1;
+		} else if (state == 1) {
+			DriveTrain.disable();
+			Elevator.setPosition(0);
+			DriveTrain.resetEncoders();
+		}
+	} 
+} 
