1e107f0ce21f8c116583fb9aebaf0462c57da0e2
Team293 <293.First@gmail.com>
2015-02-16 12:17:14 -0500

    :zap: Daniel
    Fixed ToteDriveStraight code

--- src/autonomous/ToteDriveStraight.java
+++ src/autonomous/ToteDriveStraight.java
@@ -9,6 +9,7 @@ public class ToteDriveStraight {
 	
 	public static void init() {
 		DriveTrain.enable();
+		DriveTrain.resetEncoders();
 	}
 	
 	public static void drive() {
@@ -17,9 +18,9 @@ public class ToteDriveStraight {
 		if (state == 0)  {
 			DriveTrain.tankDrive(.5, .5);
 			state = 1;
-		} else if (state == 1) {
+		} else if (state == 1 && distance > 132) {
 			DriveTrain.disable();
-			Elevator.setPosition(0);
+			Elevator.setPosition(1);
 			DriveTrain.resetEncoders();
 		}
 	} 
